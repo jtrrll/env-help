cc034d6866887a8d9005b56db9770a0d9ade9069
Jackson Terrill <jacksonterrill3@gmail.com>
2024-09-03 03:17:11 -0400

    build: set up basic flake and repository

---
+++ .github/CONTRIBUTING.md
@@ -0,0 +1,14 @@
+# Contributing
+
+When contributing to this repository, please first open an issue to start a
+discussion about your contribution.
+
+We have a code of conduct, please follow it in all your interactions with the project.
+
+## Pull Request Process
+
+1. Ensure your changes work locally, are well documented, and well tested.
+2. Open a Pull Request with a descriptive, succinct title.
+Add any comments and tags you deem necessary.
+3. You may squash merge the Pull Request once the number of required
+approvals is met and all required status checks pass.
---
+++ modules/devenv.nix
@@ -0,0 +1,57 @@
+{inputs, ...}: {
+  imports = [
+    inputs.devenv.flakeModule
+  ];
+  perSystem = {pkgs, ...}: {
+    devenv.shells.default = {
+      enterShell = ''
+        printf "                 _        _
+         ___ _ ___ _____| |_  ___| |_ __
+        / -_) ' \ V /___| ' \/ -_) | '_ \\
+        \___|_||_\_/    |_||_\___|_| .__/
+                                   |_|\n" | ${pkgs.lolcat}/bin/lolcat
+        printf "\033[0;1;36mDEVSHELL ACTIVATED\033[0m\n"
+      '';
+      languages = {
+        nix.enable = true;
+      };
+      packages = [
+        pkgs.commitizen
+      ];
+      pre-commit = {
+        default_stages = ["pre-push"];
+        hooks = {
+          actionlint.enable = true;
+          alejandra.enable = true;
+          check-added-large-files = {
+            enable = true;
+            stages = ["pre-commit"];
+          };
+          check-yaml.enable = true;
+          commitizen.enable = true;
+          deadnix.enable = true;
+          detect-private-keys = {
+            enable = true;
+            stages = ["pre-commit"];
+          };
+          end-of-file-fixer.enable = true;
+          flake-checker.enable = true;
+          markdownlint.enable = true;
+          mixed-line-endings.enable = true;
+          nil.enable = true;
+          no-commit-to-branch = {
+            enable = true;
+            stages = ["pre-commit"];
+          };
+          ripsecrets = {
+            enable = true;
+            stages = ["pre-commit"];
+          };
+          shellcheck.enable = true;
+          shfmt.enable = true;
+          statix.enable = true;
+        };
+      };
+    };
+  };
+}
